504683656692973158c896e8f63346762b65d4e0
ddsooxo <dchuteng@gmail.com>
2020-11-17 23:02:42 -0800

    adding all files, currently at 3:24:03 mark in the course

---
+++ src/books.js
@@ -0,0 +1,27 @@
+
+
+export const data = [
+    {
+        id: 1,
+        img:
+            "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+        title: "I Love You to the Moon and Back",
+        author: "Amelia Hepworth ",
+    },
+
+    {
+        id: 2,
+        img:
+            "https://images-na.ssl-images-amazon.com/images/I/91vtkaBWXeL._AC_UL200_SR200,200_.jpg",
+        title: "How to Catch a Unicorn",
+        author: "Adam Wallace",
+    },
+
+    {
+        id: 3,
+        img:
+            "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+        title: "A Promise Land",
+        author: "Barack Obama",
+    },
+];
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import "./index.css";
+import { data } from "./books";
+import Book from "./Book";
+
+import { greeting } from "./testing/testing";
+
+function BookList() {
+  console.log(greeting);
+  return (
+    <section className="booklist">
+      {data.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
